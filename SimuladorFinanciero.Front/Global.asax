26aaae3fe865726fc266bc6183645616467cd36b
CcarlosSnow <ccarlos_snow@hotmail.com>
2016-07-04 20:01:53 -0500

    Cambio de Nombres

---
+++ SimuladorFinanciero.Front/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimuladorFinanciero.Front.MvcApplication" Language="C#" %>
---
+++ SimuladorFinanciero.Front/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimuladorFinanciero.Front.Startup))]
+namespace SimuladorFinanciero.Front
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
